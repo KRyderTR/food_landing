266006dd79289ca71414e4ddb6132f94c4cd9a5b
KRyderTR <charitrom@gmail.com>
2025-05-31 23:24:16 +0300

    initial commit

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -3,7 +3,7 @@ import { MdDeliveryDining } from "react-icons/md";
 
 const Nav = () => {
   return (
-    <div className="transition-all duration-200 h-[12vh] z-[100] fixed w-full">
+    <div className="transition-all duration-200 h-[12vh] z-[100] fixed">
         <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
             {/* LOGO */}
             <div className="flex items-center space-x-2">
