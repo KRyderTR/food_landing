c7b23ea8a15918ca5a462074af0b04a108af3e59
KRyderTR <charitrom@gmail.com>
2025-06-03 10:41:25 +0300

    restSectionComplete

---
+++ components/Home/Restaurant/Restaurant.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import RestaurantCard from "./RestaurantCard";
+import { RestaurantDemoData } from "@/constant/restaurant-demo-data";
+
+const Restaurant = () => {
+  const demoList = RestaurantDemoData;
+
+  return (
+    <div className="pt-16 pb-16">
+      <div className="text-xl sm:text-2xl text-center font-extrabold">
+        Available Restaurant Nearby Area
+      </div>
+
+      <div className="w-[80%] mx-auto grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10 mt-14">
+        {demoList.map((data) => (
+          <RestaurantCard key={data.id} image={data.image} title={data.title} />
+        ))}
+      </div>
+
+      <div className="mt-16 text-center">
+        <button className="px-9 py-2 sm:px-12 sm:py-3.5 cursor-pointer rounded-full font-bold text-base sm:text-lg bg-cyan-700 text-white dark:text-black hover:bg-cyan-950 transition-all duration-300 dark:bg-cyan-300 dark:hover:bg-cyan-200">
+          Discover More &rarr;
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Restaurant;
---
+++ constant/restaurant-demo-data.ts
@@ -0,0 +1,32 @@
+export const RestaurantDemoData = [
+    {
+        id: 1,
+        image: "/images/r1.jpg",
+        title: "Pizza Hut Delicius Pizza",
+    },
+    {
+        id: 2,
+        image: "/images/r2.jpg",
+        title: "Chipotle Mexican Grill (2675 Geary Boulevard)",
+    },
+    {
+        id: 3,
+        image: "/images/r3.jpg",
+        title: "McDonald's® Burgers (Fillmore)",
+    },
+    {
+        id: 4,
+        image: "/images/r4.jpg",
+        title: "The Baked Bear San Francisco",
+    },
+    {
+        id: 5,
+        image: "/images/r5.jpg",
+        title: "Shake Shack (3060 Fillmore Street)",
+    },
+    {
+        id: 6,
+        image: "/images/r6.jpg",
+        title: "Chubby Noodle Chinese Takeout",
+    },
+]
